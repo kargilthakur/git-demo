1756b9315699e3fe2cac034135aa9d0ce9e68220
kargilthakur <49880502+kargilthakur@users.noreply.github.com>
2021-06-03 14:38:01 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,22 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2580
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\csgenericrgb\c0\c0\c0;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww28600\viewh14920\viewkind0
+\deftab720
+\pard\pardeftab720\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf2 #add function\
+def add(x,y):\
+	pass\
+#subtract function\
+def subtract(x,y):\
+    pass\
+#multiply function\
+def multiply(x,y):\
+	pass\
+#divide function\
+def divide(x,y):\
+    pass\
+\
+}
